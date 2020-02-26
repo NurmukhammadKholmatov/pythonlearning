60f61bb931c6ffe76e86d6be179fa5342bbda996
Nurmukhammad <ripplenur@gmail.com>
2020-02-26 17:09:57 +0500

    os info

---
+++ 01_os_info.py
@@ -0,0 +1,14 @@
+#-*- conding: utf-8 -*-
+# Нужно собрать информацию об операционной системе и версии пайтона
+
+
+import os
+import platform
+import sys
+
+
+info = 'Os info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+ with open('os_info.txt', 'w') as ff:
+     ff.write(info)
