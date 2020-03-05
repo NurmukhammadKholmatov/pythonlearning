c3446d9201c9aedf3789042f2988caa44a8ab369
Nurmukhammad <ripplenur@gmail.com>
2020-03-05 11:08:29 +0500

    distances hisoblash

--- Lesson_2/00_distance.py
+++ Lesson_2/00_distance.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
-
+from pprint import pprint
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -12,11 +12,25 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = dict()
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
 
-# TODO здесь заполнение словаря
+moscow_london = ((moscow[0] - london[0])**2 + (moscow[1] - london[1])**2)**.5
+moscow_paris = ((moscow[0] - paris[0])**2 + (moscow[1] - paris[1])**2)**.5
+london_paris = ((london[0] - paris[0])**2 + (london[1] - paris[1])**2)**.5
 
-print(distances)
+distances['Moscow'] = {}
+distances['London'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+distances['London']['Paris'] = london_paris
+distances['London']['Moscow'] = moscow_london
+
+pprint(distances)
+
+pprint(distances ['Moscow'])
 
 
 
